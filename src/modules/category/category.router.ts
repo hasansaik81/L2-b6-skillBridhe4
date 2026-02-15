f03d6ade0f0022c06c14b9088d9c49e3fdf216c3
Hasan Saik <hasansaik81@gmail.com>
2026-02-16 00:22:29 +0600

    delete category router

--- src/modules/category/category.router.ts
+++ src/modules/category/category.router.ts
@@ -12,3 +12,8 @@ router.post("/subject/create",auth(UserRoles.ADMIN),categoryController.createSub
 
 router.put("/update/:categoryId",auth(UserRoles.ADMIN),categoryController.updateCategory)
 router.put("/update/subject/:subjectId",auth(UserRoles.ADMIN),categoryController.updateSubject)
+
+router.delete("/delete/:categoryId",auth(UserRoles.ADMIN),categoryController.deleteCategory)
+router.delete("/delete/subject/:subjectId",auth(UserRoles.ADMIN),categoryController.deleteSubject)
+
+export const categoryRouter=router
